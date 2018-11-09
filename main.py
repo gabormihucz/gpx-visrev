6c9ac4d9c696a3fa3a84a537b6cf7b328a8a3dbf
magnarch <magnarch@gmail.com>
2018-11-09 16:12:54 +0000

    finished unfinished function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import gpxpy.gpx
+from datetime import datetime
 
 
 def average_speed(location_list):
@@ -14,12 +15,18 @@ def max_speed(location_list):
 	return (max(speed_list))
 
 def start_time(location_list):
-	return location_list[0][4]
+	return location_list[0][4][11:-1]
 
 def end_time(location_list):
 	return location_list[len(location_list)-1][4]
 
-def time_difference
+def time_difference(location_list):
+
+	s1 = start_time(location_list)[11:-1]
+	s2 = end_time(location_list)[11:-1]
+	FMT = '%H:%M:%S'
+	tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
+	return tdelta
 
 
 def get_data(file_name):
@@ -35,5 +42,5 @@ def get_data(file_name):
 
     return points
 
-# import pprint
-# pprint.pprint(get_data("resources/data/Lugano.gpx"))
+import pprint
+pprint.pprint(get_data("resources/data/cycling.gpx"))
