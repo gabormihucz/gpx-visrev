9e6b20cded5eec9e0ff102f6266f0e094d8370fb
magnarch <magnarch@gmail.com>
2018-11-09 15:42:55 +0000

    adding parsing code done so far

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import gpxpy.gpx
 
 
+def average_speed(location_list):
+	speed_list=[]
+	for location in location_list:
+		speed_list.append(location[3])
+	return (sum(speed_list)/len(speed_list))
+
+def max_speed(location_list):
+	speed_list=[]
+	for location in location_list:
+		speed_list.append(location[3])
+	return (max(speed_list))
+
+def start_time(location_list):
+	return location_list[0][4]
+
+def end_time(location_list):
+	return location_list[len(location_list)-1][4]
+
+def time_difference
+
+
 def get_data(file_name):
     points = {}
     with open(file_name, "r") as gpx_file:
@@ -10,7 +31,7 @@ def get_data(file_name):
         points[track.name] = []
         for seg in track.segments:
             for point in seg.points:
-                points[track.name].append((point.latitude, point.longitude, point.elevation))
+                points[track.name].append((point.latitude, point.longitude, point.elevation, point.speed, point.time))
 
     return points
 
