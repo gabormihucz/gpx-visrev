e876fb10776c1643ccb32999d71cfc5fd4c4645c
macutko <matusgallik008@gmail.com>
2018-11-16 18:46:35 +0000

    lets try it fuckin finally

--- js/chart.js
+++ js/chart.js
@@ -1,36 +1,27 @@
 window.onload = function () {
+new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+	 
+    labels: [1,2,3,4,5,6],
+    datasets: [{ 
+        data: [281.600006103515625,281.600006103515625,280.20001220703125,280.20001220703125,280.20001220703125,280.20001220703125],
+        label: "Track1",
+        borderColor: "#3e95cd",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Elevation of the selected track',
+    },
+	responsive: true,
+	maintainAspectRatio: false,
+  }
+});
 
-var canvas = document.getElementById('canvas'),
-		context = canvas.getContext('2d'),
-		width = canvas.width = 800,
-		height = canvas.height = 400;
-
-var stats = [40, 65, 72, 120, 210, 87, 100, 42];
-
-context.translate(0, height);
-context.scale(1, -1);
-
-context.fillStyle = '#ffffff';
-context.fillRect(0, 0, width, height);
-
-var left = 0,
-		prev_stat = stats[0],
-		move_left_by = 100;
-
-for(stat in stats) {
-	the_stat = stats[stat];
-
-	context.beginPath();
-	context.moveTo(left, prev_stat);
-	context.lineTo(left+move_left_by, the_stat);
-	context.lineWidth = 5;
-	context.lineCap = 'round';
-
-	context.stroke();
-
-	prev_stat = the_stat;
-	left += move_left_by;
-
-}
+var currentWindowHeight = $(window).height()
 
 }
