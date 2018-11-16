f4d1b44db7f98dc91c3a90fc6f051612bfc6b44c
polarhun <szviktor10@gmail.com>
2018-11-16 16:03:29 +0000

    Basic GPx plotting

--- js/fileReader.js
+++ js/fileReader.js
@@ -1,20 +1,20 @@
 window.onload = function() {
-    var fileInput = document.getElementById('fileInput');
-    fileInput.addEventListener('change', function(e) {
-      var file = fileInput.files[0];
-      var fileType = ".gpx";
-      if (file.name.includes(fileType)) {
-        var reader = new FileReader();
+  var fileInput = document.getElementById("fileInput");
+  fileInput.addEventListener("change", function(e) {
+    var file = fileInput.files[0];
+    var fileType = ".gpx";
+    if (file.name.includes(fileType)) {
+      var reader = new FileReader();
 
-        reader.onload = function(e) {
-          var dom = (new DOMParser()).parseFromString(reader.result, 'text/xml');
-          var json = toGeoJSON.gpx(dom);
-          populateMap(json);
-        }
+      reader.onload = function(e) {
+        var dom = new DOMParser().parseFromString(reader.result, "text/xml");
+        var json = toGeoJSON.gpx(dom);
+        populateMap(json);
+      };
 
-        reader.readAsBinaryString(file);
-      } else {
-        fileDisplayArea.innerText = "File not supported!"
-      }
-    });
-}
+      reader.readAsBinaryString(file);
+    } else {
+      fileDisplayArea.innerText = "File not supported!";
+    }
+  });
+};
--- js/map.js
+++ js/map.js
@@ -1,42 +1,57 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicG9sYXJodW4iLCJhIjoiY2pvYTZhZWJ5MGIwZjNrbXBiNTd3amJnOSJ9.xIn7UgAKh1kF2zTRc6xnxA";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v10",
+  center: [-8.044135, 37.063142],
+  zoom: 12
+});
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoicG9sYXJodW4iLCJhIjoiY2pvYTZhZWJ5MGIwZjNrbXBiNTd3amJnOSJ9.xIn7UgAKh1kF2zTRc6xnxA';
-    var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-8.044135, 37.063142],
-    zoom: 16
+function populateMap(data) {
+  /*
+   * Function takes the data, flies to first coordinate, and plots all coordinates.
+   * It also removes previous plot from the map, we need to delete both the layer, and the source.
+   */
+
+  // data is the GeoJSON file, extracted is the first data point of longitude, latitude, elevation
+  // data.features[0].geometry.coordinates is the set of all long-lat-ele data points from the GPX
+  // For time and heartbeat and other data, more digging is needed in the GeoJSON
+  var extracted = data.features[0].geometry.coordinates;
+  var coord = [];
+  extracted.forEach(element => {
+    coord.push([element[0], element[1]]);
+  });
+  map.flyTo({
+    center: [extracted[0][0], extracted[0][1]],
+    zoom: 14
   });
 
-  function populateMap(data){
-    // data is the GeoJSON file, extracted is the first data point of longitude, latitude, elevation
-    // data.features[0].geometry.coordinates is the set of all long-lat-ele data points from the GPX
-    // For time and heartbeat and other data, more digging is needed in the GeoJSON
-    var extracted = (data.features[0].geometry.coordinates[0]);
-    console.log(extracted);
-    map.flyTo({
-          center: [extracted[0], extracted[1]]
-      });
-    // map.addLayer({
-    //     "id": "route",
-    //     "type": "line",
-    //     "source": {
-    //         "type": "geojson",
-    //         "data": {
-    //             "type": "Feature",
-    //             "properties": {},
-    //             "geometry": {
-    //                 "type": "LineString",
-    //                 "coordinates": coord
-    //             }
-    //         }
-    //     },
-    //     "layout": {
-    //         "line-join": "round",
-    //         "line-cap": "round"
-    //     },
-    //     "paint": {
-    //         "line-color": "#ff69b4",
-    //         "line-width": 8
-    //     }
-    // });
+  if (map.getLayer("route") !== undefined) {
+    map.removeLayer("route");
+    map.removeSource("route");
   }
+
+  map.addLayer({
+    id: "route",
+    type: "line",
+    source: {
+      type: "geojson",
+      data: {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          type: "LineString",
+          coordinates: coord
+        }
+      }
+    },
+    layout: {
+      "line-join": "round",
+      "line-cap": "round"
+    },
+    paint: {
+      "line-color": "#ff69b4",
+      "line-width": 4
+    }
+  });
+}
