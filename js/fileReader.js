ececc088d07a8a014e635f8eedbb23608ed69d0e
polarhun <szviktor10@gmail.com>
2018-11-17 10:56:09 +0000

    Initital map change

--- js/fileReader.js
+++ js/fileReader.js
@@ -1,19 +1,35 @@
-  var fileInput = document.getElementById("fileInput");
-  fileInput.addEventListener("change", function(e) {
-    var file = fileInput.files[0];
-    var fileType = ".gpx";
-    if (file.name.includes(fileType)) {
-      var reader = new FileReader();
+var fileInput = document.getElementById("fileInput");
 
-      reader.onload = function(e) {
-        var dom = new DOMParser().parseFromString(reader.result, "text/xml");
-        var json = toGeoJSON.gpx(dom);
-        populateMap(json);
-      };
+var createQueue = function(length) {
+  var pointer = 0,
+    buffer = [];
 
-      reader.readAsBinaryString(file);
-    } else {
-      fileDisplayArea.innerText = "File not supported!";
+  return {
+    get: function(index) {
+      return buffer[index];
+    },
+    push: function(item) {
+      buffer[pointer] = item;
+      pointer = (length + pointer + 1) % length;
     }
-  });
+  };
+};
 
+fileInput.addEventListener("change", function(e) {
+  var queue = createQueue(5);
+  var file = fileInput.files[0];
+  var fileType = ".gpx";
+  if (file.name.includes(fileType)) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      var dom = new DOMParser().parseFromString(reader.result, "text/xml");
+      var json = toGeoJSON.gpx(dom);
+      populateMap(json, 0);
+    };
+
+    reader.readAsBinaryString(file);
+  } else {
+    fileDisplayArea.innerText = "File not supported!";
+  }
+});
--- js/map.js
+++ js/map.js
@@ -7,7 +7,15 @@ var map = new mapboxgl.Map({
   zoom: 12
 });
 
-function populateMap(data) {
+function popLayer(index) {
+  var currentRoute = "route" + index;
+  if (map.getLayer(currentRoute) !== undefined) {
+    map.removeLayer(currentRoute);
+    map.removeSource(currentRoute);
+  }
+}
+
+function populateMap(data, index) {
   /*
    * Function takes the data, flies to first coordinate, and plots all coordinates.
    * It also removes previous plot from the map, we need to delete both the layer, and the source.
@@ -18,23 +26,10 @@ function populateMap(data) {
   // data.features[0].geometry.coordinates is the set of all long-lat-ele data points from the GPX
   // For time and heartbeat and other data, more digging is needed in the GeoJSON
   const extracted = data.features[0].geometry.coordinates;
-  const pDensity = 30;
   var coord = [];
-  var points = [];
-  var index = 0;
+  var currentRoute = "route" + index;
   extracted.forEach(element => {
     coord.push([element[0], element[1]]);
-    if (index % pDensity === 0) {
-      points.push({
-        type: "Feature",
-        properties: {},
-        geometry: {
-          type: "Point",
-          coordinates: [element[0], element[1]]
-        }
-      });
-    }
-    index++;
   });
 
   /** fly to the track and fit the map perfectly to it*/
@@ -55,15 +50,10 @@ function populateMap(data) {
     padding: { top: 10, bottom: 25, left: 15, right: 5 }
   });
 
-  if (map.getLayer("route") !== undefined) {
-    map.removeLayer("route");
-    map.removeSource("route");
-    map.removeLayer("points");
-    map.removeSource("points");
-  }
+  popLayer(index);
 
   map.addLayer({
-    id: "route",
+    id: currentRoute,
     type: "line",
     source: {
       type: "geojson",
@@ -84,32 +74,5 @@ function populateMap(data) {
       "line-color": "#ff69b4",
       "line-width": 4
     }
-  });
-
-  map.addLayer({
-    id: "points",
-    type: "circle",
-    source: {
-      type: "geojson",
-      data: {
-        type: "FeatureCollection",
-        features: points
-      }
-    },
-    paint: {
-      "circle-color": "#32cd32"
-    }
-  });
-
-  map.on("click", "points", function(e) {
-    map.flyTo({ center: e.features[0].geometry.coordinates });
-  });
-
-  map.on("mouseenter", "points", function() {
-    map.getCanvas().style.cursor = "pointer";
-  });
-
-  map.on("mouseleave", "points", function() {
-    map.getCanvas().style.cursor = "";
   });
 }
