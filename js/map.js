5469d1663641c7ac2f1f8da4715d35ddf9942419
magnarch <magnarch@gmail.com>
2018-11-17 00:18:39 +0000

    adding extra comment

--- js/map.js
+++ js/map.js
@@ -62,6 +62,8 @@ function populateMap(data) {
 
 
 /** get distances between measured points and put them in an array */
+/** have doubts about the results, need to be checked that this is working properly 
+**  speeds seem to be way too low */
 
 function distance(lat1, lon1, lat2, lon2) {
   var p = 0.017453292519943295;    // Math.PI / 180
@@ -86,6 +88,8 @@ for (i = 1; i < extracted_properties.coordTimes.length; i++) {
         d2 = new Date(extracted_properties.coordTimes[i]);
         times.push(d2-d1);
 } 
+
+/** divide distance by time to get speed */
 
 speeds=[]
 var i;
