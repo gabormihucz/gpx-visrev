1c99986741de972a95d792d7d9b898b7518dc4c6
Jan Paralic <jan.paralic@exponea.com>
2018-11-20 14:05:22 +0000

    Add font size accessibility options

---
+++ js/accessibility.js
@@ -0,0 +1,23 @@
+$('#default-font-size').click(function() {
+    $(".detailed").css("font-size", "1em");
+    $(".navbar-brand").css("font-size", "2em");
+    $(".btn").css("font-size", "20px");
+    $(".fa-upload").css("font-size", "20px");
+    $(".legend").css("font-size", "0.8em");
+})
+
+$('#large-font-size').click(function() {
+    $(".detailed").css("font-size", "1.5em");
+    $(".navbar-brand").css("font-size", "3em");
+    $(".btn").css("font-size", "30px");
+    $(".fa-upload").css("font-size", "30px");
+    $(".legend").css("font-size", "1.2em");
+})
+
+$('#huge-font-size').click(function() {
+    $(".detailed").css("font-size", "2em");
+    $(".navbar-brand").css("font-size", "4em");
+    $(".btn").css("font-size", "40px");
+    $(".fa-upload").css("font-size", "40px");
+    $(".legend").css("font-size", "1.6em");
+})
