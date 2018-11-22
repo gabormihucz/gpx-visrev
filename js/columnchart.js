befc732a367ae8f9e18905be54105bd8e57c539f
Gabor Mihucz <2239495m@bo720-2-01u.dcs.gla.ac.uk>
2018-11-22 17:44:46 +0000

    pie chart conversion

--- js/columnchart.js
+++ js/columnchart.js
@@ -4,17 +4,15 @@ var chart = new CanvasJS.Chart(document.getElementById("chartContainer"), {
 	animationEnabled: true,
 	theme: "light2", // "light1", "light2", "dark1", "dark2"
 	title:{
-		text: "Summary statistics"
+		text: "Distance travelled"
 	},
-	axisY: {
-		title: "Distance travelled"
-	},
-	data: [{        
-		type: "column",  
-		showInLegend: true, 
-		legendMarkerColor: "grey",
-		legendText: "Measured in meters",
-		dataPoints: [      
+
+	data: [{
+		type: "pie",
+		startAngle: 240,
+		yValueFormatString: "##\" m\"",
+		indexLabel: "{label} {y}",
+		dataPoints: [
 			{ y: up[0], label: "Uphill" },
 			{ y: up[1],  label: "Downhill" },
 			{ y: up[2],  label: "Horizontally" },
