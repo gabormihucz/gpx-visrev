3cfa5229c3540c51429355bb8af24678d8fd4fbb
Viktor Szendrei <2253032s@student.gla.ac.uk>
2018-11-30 12:28:33 +0000

    Modified colour range a bit

--- js/fileReader.js
+++ js/fileReader.js
@@ -93,7 +93,7 @@ document.getElementById("flyTo").addEventListener("click", function(e) {
 });
 
 function getColour() {
-  var letters = "23456789ABCD";
+  var letters = "456789ABCD";
   var colour = "#";
   for (var i = 0; i < 6; i++) {
     colour += letters[Math.floor(Math.random() * letters.length)];
